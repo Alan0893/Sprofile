30f59cf128aaa9fb3bd4e274e101542ac87c341c
alan <alanl07905@gmail.com>
2024-03-11 15:05:20 -0400

    setup backend

---
+++ server/index.js
@@ -0,0 +1,152 @@
+// Loading environment variables for the .env file into process.env
+const path = require('path');
+const dotenv = require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+// Getting environment variables
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const CLIENT_URI = process.env.CLIENT_URI;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const PORT = process.env.PORT || 3000;
+
+// Required modules
+const express = require('express');
+const request = require('request');
+const cors = require('cors');
+const crypto = require('crypto');
+const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+
+// Creating express app
+const app = express();
+
+// Priority serve any static files
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, '../client/build')))
+  .use(cors())
+  .use(cookieParser())
+  .use(
+    history({
+      verbose: true,
+      rewrites: [
+        { from: /\/login/, to: '/login' },
+        { from: /\/callback/, to: '/callback' },
+        { from: /\/refresh_token/, to: '/refresh_token' },
+      ],
+    }),
+  )
+  .use(express.static(path.resolve(__dirname, '../client/build')));
+
+// SPOTIFY WORK
+const generateRandomString = (length) => {
+  return crypto
+  .randomBytes(60)
+  .toString('hex')
+  .slice(0, length);
+}
+
+const stateKey = 'spotify_auth_state';
+
+// ROUTING **************************************************************
+// Default Home Page
+app.get('/', function (req, res) {
+  res.render(path.resolve(__dirname, '../client/build/index.html'));
+});
+
+// Login Page
+app.get('/login', function(req, res) {
+	const state = generateRandomString(16);
+	res.cookie(stateKey, state);
+
+	// Authorization request
+	const scope =
+    'user-read-private user-read-email user-read-recently-played user-top-read user-follow-read user-follow-modify playlist-read-private playlist-read-collaborative playlist-modify-public';
+
+  res.redirect(
+    `https://accounts.spotify.com/authorize?${querystring.stringify({
+      response_type: 'code',
+      client_id: CLIENT_ID,
+      scope: scope,
+      redirect_uri: REDIRECT_URI,
+      state: state,
+    })}`,
+  );
+})
+
+
+// Callback Page -  Getting Data from the API
+app.get('/callback', function (req, res) {
+  const code = req.query.code || null;
+  const state = req.query.state || null;
+  const storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (state === null || state !== storedState) {
+    res.redirect(`/#${querystring.stringify({ error: 'state_mismatch' })}`);
+  } else {
+    res.clearCookie(stateKey);
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: REDIRECT_URI,
+        grant_type: 'authorization_code',
+      },
+      headers: {
+        Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+          'base64',
+        )}`,
+      },
+      json: true,
+    };
+
+    request.post(authOptions, function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        const access_token = body.access_token;
+        const refresh_token = body.refresh_token;
+
+        res.redirect(
+          `${CLIENT_URI}/#${querystring.stringify({
+            access_token,
+            refresh_token,
+          })}`,
+        );
+      } else {
+        res.redirect(`/#${querystring.stringify({ error: 'invalid_token' })}`);
+      }
+    });
+  }
+});
+
+// Refresh Token
+app.get('/refresh_token', function (req, res) {
+  const refresh_token = req.query.refresh_token;
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      Authorization: `Basic ${new Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString(
+        'base64',
+      )}`,
+    },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token,
+    },
+    json: true,
+  };
+
+  request.post(authOptions, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const access_token = body.access_token;
+      res.send({ access_token });
+    }
+  });
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, '../client/public', 'index.html'));
+});
+
+app.listen(PORT, function () {
+  console.warn(`Listening on port ${PORT}`);
+});
