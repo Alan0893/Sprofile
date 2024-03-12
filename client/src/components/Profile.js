f9fdfc1baeac43261a5bdf5db496e6cde96d5650
alan <alanl07905@gmail.com>
2024-03-11 21:17:39 -0400

    added profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,16 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import ScrollToTop from './ScrollToTop';
+import Nav from './Nav';
 import User from './User';
+import RecentlyPlayed from './RecentlyPlayed';
+import TopArtists from './TopArtists';
+import TopTracks from './TopTracks';
+import Playlists from './Playlists';
+import Playlist from './Playlist';
+import Recommendations from './Recommendations';
+import Track from './Track';
+import Artist from './Artist';
 
 import styled from 'styled-components';
 import { theme, media } from '../styles';
@@ -15,11 +25,19 @@ const SiteWrapper = styled.div`
 
 const Profile = () => (
 	<SiteWrapper>
-		<Router>
-			<Switch>
-				<Route exact path="/" component={User} />
-			</Switch>
-		</Router>
+		<Nav/>
+			<ScrollToTop/>
+			<Routes>
+				<Route exact path="/" element={<User/>} />
+				<Route path="/recent" element={<RecentlyPlayed/>} />
+				<Route path="/artists" element={<TopArtists/>} />
+				<Route path="/tracks" element={<TopTracks/>} />
+				<Route path="/playlists" element={<Playlists/>} />
+				<Route path="/playlist/:id" element={<Playlist/>} />
+				<Route path="/recommendations/:id" element={<Recommendations/>} />
+				<Route path="/track/:id" element={<Track/>} />
+				<Route path="/artist/:id" element={<Artist/>} />
+			</Routes>
 	</SiteWrapper>
 )
 
