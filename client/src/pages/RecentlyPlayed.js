b22ce598e699ed165247bdb3ab15a2c9d26343d4
alan <alanl07905@gmail.com>
2024-03-13 18:28:55 -0400

    minor edits

--- client/src/pages/RecentlyPlayed.js
+++ client/src/pages/RecentlyPlayed.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { getRecentlyPlayed } from '../api/player';
 import { catchErrors } from '../utils';
 
