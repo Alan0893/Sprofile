e93432e6fbebc429c56970b3307df169c725fa6f
alan <alanl07905@gmail.com>
2024-03-13 11:19:54 -0400

    updated and added endpoints

--- client/src/api/playlist.js
+++ client/src/api/playlist.js
@@ -9,6 +9,69 @@ const headers = {
 
 //*********************************************************************************************************************************
 /**
+ * Get a Playlist
+ * https://developer.spotify.com/documentation/web-api/reference/get-playlist/
+ */
+export const getPlaylist = (playlistId) =>
+  axios.get(`https://api.spotify.com/v1/playlists/${playlistId}`, { 
+		headers 
+	});
+
+//*********************************************************************************************************************************
+/**
+ * Change Playlist Details
+ * https://developer.spotify.com/documentation/web-api/reference/change-playlist-details/
+ */
+export const changePlaylistDetails = (playlistId, name, description, isPublic) => {
+  const url = `https://api.spotify.com/v1/playlists/${playlistId}`;
+  const data = JSON.stringify({ name, description, public: isPublic });
+  return axios({ method: 'put', url, headers, data });
+}
+
+//*********************************************************************************************************************************
+/**
+ * Get a Playlist Items
+ * https://developer.spotify.com/documentation/web-api/reference/get-playlists-tracks/
+ */
+export const getPlaylistTracks = (playlistId) =>
+  axios.get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, { 
+    headers 
+  });
+
+//*********************************************************************************************************************************
+/**
+ * Update Playlist Items
+ * https://developer.spotify.com/documentation/web-api/reference/reorder-or-replace-playlists-tracks
+ */
+export const updatePlaylistItems = (playlistId, range_start, insert_before, range_length) => {
+  const url = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
+  const data = JSON.stringify({ range_start, insert_before, range_length });
+  return axios({ method: 'put', url, headers, data });
+}
+
+//*********************************************************************************************************************************
+/**
+ * Add Items to a Playlist
+ * https://developer.spotify.com/documentation/web-api/reference/add-tracks-to-playlist/
+ */
+export const addTracksToPlaylist = (playlistId, uris) => {
+  const url = `https://api.spotify.com/v1/playlists/${playlistId}/tracks?uris=${uris}`;
+  return axios({ method: 'post', url, headers });
+};
+
+//*********************************************************************************************************************************
+/**
+ * Remove Playlist Items
+ * https://developer.spotify.com/documentation/web-api/reference/remove-tracks-playlist/
+ */
+export const removeTracksFromPlaylist = (playlistId, tracks) => {
+  const url = `https://api.spotify.com/v1/playlists/${playlistId}/tracks`;
+  const data = JSON.stringify({ tracks });
+  return axios({ method: 'delete', url, headers, data });
+}
+
+//*********************************************************************************************************************************
+/**
  * Get Current User's Playlists
  * https://developer.spotify.com/documentation/web-api/reference/get-a-list-of-current-users-playlists/
  */
@@ -16,6 +79,16 @@ export const getPlaylists = () =>
 	axios.get('https://api.spotify.com/v1/me/playlists', { 
 		headers 
 	});
+
+//*********************************************************************************************************************************
+/**
+ * Get User's Playlists
+ * https://developer.spotify.com/documentation/web-api/reference/get-list-users-playlists
+ */
+export const getUserPlaylists = (userId) =>
+  axios.get(`https://api.spotify.com/v1/users/${userId}/playlists`, { 
+    headers 
+  });
 
 //*********************************************************************************************************************************
 /**
@@ -30,32 +103,41 @@ export const createPlaylist = (userId, name) => {
 
 //*********************************************************************************************************************************
 /**
- * Add Tracks to a Playlist
- * https://developer.spotify.com/documentation/web-api/reference/add-tracks-to-playlist/
+ * Get Featured Playlists
+ * https://developer.spotify.com/documentation/web-api/reference/get-featured-playlists
  */
-export const addTracksToPlaylist = (playlistId, uris) => {
-  const url = `https://api.spotify.com/v1/playlists/${playlistId}/tracks?uris=${uris}`;
-  return axios({ method: 'post', url, headers });
-};
-
-//*********************************************************************************************************************************
-/**
- * Get a Playlist
- * https://developer.spotify.com/documentation/web-api/reference/get-playlist/
- */
-export const getPlaylist = (playlistId) =>
-  axios.get(`https://api.spotify.com/v1/playlists/${playlistId}`, { 
-		headers 
-	});
-
-//*********************************************************************************************************************************
-/**
- * Get a Playlist's Tracks
- * https://developer.spotify.com/documentation/web-api/reference/get-playlists-tracks/
- */
-export const getPlaylistTracks = (playlistId) =>
-  axios.get(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, { 
+export const getFeaturedPlaylists = () =>
+  axios.get('https://api.spotify.com/v1/browse/featured-playlists', { 
     headers 
   });
 
+//*********************************************************************************************************************************
+/**
+ * Get Category's Playlists
+ * https://developer.spotify.com/documentation/web-api/reference/get-a-categories-playlists
+ */
+export const getCategoryPlaylists = (categoryId) =>
+  axios.get(`https://api.spotify.com/v1/browse/categories/${categoryId}/playlists`, {
+    headers
+  });
 
+//*********************************************************************************************************************************
+/**
+ * Get Playlist Cover Image
+ * https://developer.spotify.com/documentation/web-api/reference/get-playlist-cover
+ */
+export const getPlaylistCoverImage = (playlistId) =>
+  axios.get(`https://api.spotify.com/v1/playlists/${playlistId}/images`, {
+    headers
+  });
+
+//*********************************************************************************************************************************
+/**
+ * Add Custom Playlist Cover Image
+ * https://developer.spotify.com/documentation/web-api/reference/upload-custom-playlist-cover
+ */
+export const addCustomPlaylistCoverImage = (playlistId, image) => {
+  const url = `https://api.spotify.com/v1/playlists/${playlistId}/images`;
+  const data = JSON.stringify({ image });
+  return axios({ method: 'put', url, headers, data });
+};
