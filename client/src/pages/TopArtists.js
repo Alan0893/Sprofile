f8f1c231238f5c703387a8e1961877a9061be314
alan <alanl07905@gmail.com>
2024-03-13 18:39:15 -0400

    minor edits

--- client/src/pages/TopArtists.js
+++ client/src/pages/TopArtists.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
 import { getTopArtistsShort, getTopArtistsMedium, getTopArtistsLong } from '../api/user';
 import { catchErrors } from '../utils';
 
