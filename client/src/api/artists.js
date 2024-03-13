0dcde6aec5aeb331d60dc0b1b7036bf88cd92fa8
alan <alanl07905@gmail.com>
2024-03-13 12:14:11 -0400

    added endpoints

--- client/src/api/artists.js
+++ client/src/api/artists.js
@@ -8,10 +8,50 @@ const headers = {
 };
 
 /**
- * Get an Artist
+ * Get Artist
  * https://developer.spotify.com/documentation/web-api/reference/get-an-artist
  */
 export const getArtist = (artistId) =>
   axios.get(`https://api.spotify.com/v1/artists/${artistId}`, { 
 		headers 
 	});
+
+//*********************************************************************************************************************************
+/**
+ * Get Several Artists
+ * https://developer.spotify.com/documentation/web-api/reference/get-several-artists
+ */
+export const getSeveralArtists = (ids) =>
+  axios.get(`https://api.spotify.com/v1/artists?ids=${ids}`, { 
+    headers 
+  });
+
+//*********************************************************************************************************************************
+/**
+ * Get Artist's Albums
+ * https://developer.spotify.com/documentation/web-api/reference/get-artists-albums
+ */
+export const getArtistsAlbums = (artistId) =>
+  axios.get(`https://api.spotify.com/v1/artists/${artistId}/albums`, { 
+    headers 
+  });
+
+//*********************************************************************************************************************************
+/**
+ * Get Artist's Top Tracks
+ * https://developer.spotify.com/documentation/web-api/reference/get-artists-top-tracks
+ */
+export const getArtistsTopTracks = (artistId) =>
+  axios.get(`https://api.spotify.com/v1/artists/${artistId}/top-trackS`, { 
+    headers 
+  });
+
+//*********************************************************************************************************************************
+/**
+ * Get Artist's Related Artists
+ * https://developer.spotify.com/documentation/web-api/reference/get-related-artists
+ */
+export const getArtistsRelatedArtists = (artistId) =>
+  axios.get(`https://api.spotify.com/v1/artists/${artistId}/related-artists`, { 
+    headers 
+  });
