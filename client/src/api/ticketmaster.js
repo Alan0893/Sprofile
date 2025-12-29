5b025bf547217cc19d2f28a797d9469aa55251c4
alan <alanl07905@gmail.com>
2025-12-28 20:19:09 -0500

    updated

--- client/src/api/ticketmaster.js
+++ client/src/api/ticketmaster.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const url = 'http://127.0.0.1:8888/discover';
+const url = process.env.REACT_APP_BACKEND_URL 
+  ? `${process.env.REACT_APP_BACKEND_URL}/discover`
+  : 'http://127.0.0.1:8888/discover';
 
 export const getEvents = (artistName) => {
 	return axios.get(url, {
